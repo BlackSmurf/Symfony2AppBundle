b61c3030f2201527d7c72045cfc09e7d484d462f
Julien Buonocore <julien.buonocore@gmail.com>
2014-08-28 17:57:19 +0200

    correction composer.json

--- SmurfAppBundle.php
+++ SmurfAppBundle.php
@@ -2,10 +2,6 @@
 
 namespace Smurf\AppBundle\SmurfAppBundle;
 
-require "autoload.php";
-
-die(WidgetsHelper::ALERT_DANGER);
-
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class SmurfAppBundle extends Bundle {
